5745cecb0c073f72b9a702d7830562bc73785e4e
RyanGar46 <ryangar46@gmail.com>
2022-05-03 23:23:17 -0400

    Add basic page structure

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <NuxtWelcome />
+    Nav Bar
+  </div>
+  <div>
+    <header>
+      <h1>Ryan Garret's Website</h1>
+      <h2>Everything here is temporary</h2>
+    </header>
+    <div>
+      <div>Some Text</div>
+      <img src="" alt="Wide Image">
+      <div>Description</div>
+    </div>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores iusto aut ad explicabo voluptatum consequuntur doloremque? Reiciendis quam, quis eveniet 
+      laudantium repellendus nihil eius quidem, cupiditate sed, provident accusamus atque.
+    </p>
+    <footer>
+      This is the footer
+    </footer>
   </div>
 </template>
