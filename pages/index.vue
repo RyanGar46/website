e6d94826f7ee54e3532ba5da367bfc1b8ae1fe8a
RyanGar46 <ryangar46@gmail.com>
2022-05-03 23:59:17 -0400

    Reorganize page into multiple components

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup>
+  console.log("Hi this is a test message");
+
+  let buttonClicks = 0;
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Ryan Garret's Website</h1>
+      <h2>Everything here is temporary</h2>
+    </header>
+    <div>{{ buttonClicks }}</div>
+    <button>Click Me!</button>
+    <div>
+      <div>Some Text</div>
+      <img src="" alt="Wide Image">
+      <div>Description</div>
+    </div>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores iusto aut ad explicabo voluptatum consequuntur doloremque? Reiciendis quam, quis eveniet 
+      laudantium repellendus nihil eius quidem, cupiditate sed, provident accusamus atque.
+    </p>
+  </div>
+</template>
